77fcfb1b48211f0584223e39e455f154e79d0a50
Siqi Liu <siqil2@uci.edu>
2015-02-17 15:09:25 -0800

    Understood the parts of access token request url

--- EventBrite0/Parse_Access.aspx.cs
+++ EventBrite0/Parse_Access.aspx.cs
@@ -11,9 +11,23 @@ namespace EventBrite0
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string test = Request.QueryString["code"];
+            //code=THE_USERS_AUTH_CODE&client_secret=YOUR_CLIENT_SECRET&client_id=YOUR_API_KEY&grant_type=authorization_code
+            string THE_USERS_AUTH_CODE = Request.QueryString["code"];
+            string YOUR_CLIENT_SECRET = "Y3YA5HJD3EM6UX364MKPRQUCH7NDL2RSBIHG3PS3UCM3MYCB6M";
+            string YOUR_API_KEY = "YHASIDSTEN277KD7LK";
+            string authorization_code = "";
+            //string test = Request.QueryString["code"];
+            /* http://developer.eventbrite.com/docs/auth/
+             * You must then exchange this access code for an OAuth token. Send a POST request to::
 
-            Response.Write(test);
+                https://www.eventbrite.com/oauth/token
+                This POST must contain the following urlencoded data, along with a “Content-type: application/x-www-form-urlencoded“ header::
+
+                code=THE_USERS_AUTH_CODE&client_secret=YOUR_CLIENT_SECRET&client_id=YOUR_API_KEY&grant_type=authorization_code
+                The subsequent “POST“ will contain the user’s access_token.
+             */
+
+            Response.Write(THE_USERS_AUTH_CODE);
         }
     }
 }
