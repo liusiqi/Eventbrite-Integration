807e1c8ec1834c8ac20ae819552e5c374b4dd14a
Siqi Liu <siqil2@uci.edu>
2015-02-17 18:22:09 -0800

    Get the access token.

--- EventBrite0/Parse_Access.aspx.cs
+++ EventBrite0/Parse_Access.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -26,8 +28,53 @@ namespace EventBrite0
                 code=THE_USERS_AUTH_CODE&client_secret=YOUR_CLIENT_SECRET&client_id=YOUR_API_KEY&grant_type=authorization_code
                 The subsequent “POST“ will contain the user’s access_token.
              */
+            string longTermUrl = "https://www.eventbrite.com/oauth/token";
+            HttpWebRequest request = null;
+            HttpWebResponse response = null;
 
-            Response.Write(THE_USERS_AUTH_CODE);
+            try
+            {
+                //create the request
+                request = (HttpWebRequest)WebRequest.Create(longTermUrl);
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+
+                string postFields = "";
+
+                postFields = "code=" + HttpUtility.UrlEncode(THE_USERS_AUTH_CODE) + "&";
+                postFields += "client_secret=" + HttpUtility.UrlEncode(YOUR_CLIENT_SECRET) + "&";
+                postFields += "client_id=" + HttpUtility.UrlEncode(YOUR_API_KEY) + "&";
+                postFields += "grant_type=" + HttpUtility.UrlEncode("authorization_code");
+
+                //Get lenght of postfields and set to Content Length
+                request.ContentLength = postFields.Length;
+
+                //Post data is sent as a stream
+                StreamWriter sWriter = null;
+                sWriter = new StreamWriter(request.GetRequestStream());
+
+                //Add 'postfields' to request
+                sWriter.Write(postFields);
+                sWriter.Close();
+                response = (HttpWebResponse)request.GetResponse();
+                string post_response = "";
+                using (StreamReader responseStream = new StreamReader(response.GetResponseStream()))
+                {
+                    post_response = responseStream.ReadToEnd();
+                    responseStream.Close();
+                }
+
+                //Close response stream
+                response.Close();
+                Response.Write(post_response);
+            }
+            catch
+            {
+                string[] p = new string[2];
+                p[0] = "error";
+                p[1] = "Error Message (retrieving long term access token): ";
+                Response.Write(p);
+            }
         }
     }
 }
