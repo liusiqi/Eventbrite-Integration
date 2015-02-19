c37a90a4ba62a7fb4b9b0339de01756259982dc4
Siqi Liu <siqil2@uci.edu>
2015-02-19 09:37:58 -0800

    Get access token from Json string.

--- EventBrite0/Default.aspx.cs
+++ EventBrite0/Default.aspx.cs
@@ -13,9 +13,9 @@ namespace EventBrite0
         protected void Page_Load(object sender, EventArgs e)
         {
             //string first_url = "https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=YHASIDSTEN277KD7LK";
-            string BaseURL = "https://www.eventbrite.com";
-            string Resource = "/oauth/authorize";
-            string QueryString = "?response_type=code&client_id=YHASIDSTEN277KD7LK";
+            const string BaseURL = "https://www.eventbrite.com";
+            const string Resource = "/oauth/authorize";
+            const string QueryString = "?response_type=code&client_id=YHASIDSTEN277KD7LK";
             StringBuilder builder = new StringBuilder();
             builder.Append(BaseURL).Append(Resource).Append(QueryString);
             Server.ClearError();
