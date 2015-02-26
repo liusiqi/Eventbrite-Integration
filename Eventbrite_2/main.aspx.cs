b7f7cf1d378bcdab1d3b511b0b31ae61fd3066d2
Siqi Liu <siqil2@uci.edu>
2015-02-26 10:51:40 -0800

    What is going on here?

---
+++ Eventbrite_2/main.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Eventbrite_2
+{
+    public partial class main : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                API_Methods Eventbrite_Object = new API_Methods("5Z5FE6HF3BJ763FOJFOQ");
+                string userID = Eventbrite_Object.User_ID();
+                //Response.Write(userID + "\n\n");
+                string Json_pagination_events = Eventbrite_Object.User_Events(userID);
+                Dictionary<string, string> Name_Link = new Dictionary<string, string>();
+                Dictionary<string, string> Name_Draft = new Dictionary<string, string>();
+                Dictionary<string, string> Name_Ignore = new Dictionary<string, string>();
+                //Response.Write(pagination_events);
+
+                Eventbrite_Object.Create_Dictionaries(Json_pagination_events, Name_Link, Name_Draft, Name_Ignore);
+            }
+            catch (WebException me)
+            {
+                string ErrorMessage = "Error : retrieving alive event list failed. " + me;
+                Response.Write(ErrorMessage);
+            }
+        }
+    }
+}
